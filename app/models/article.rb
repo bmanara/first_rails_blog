52f09eb4c066af3d77005ab34f7f140f50f043a9
bmanara <bmacraze@gmail.com>
2023-12-24 02:43:49 +0800

    Added functionality to post comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  # associated with some comments in the comment table
+  has_many :comments
   # Validates the inputs when trying to create in db
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index" # a route that maps the root path of our app to the appropriate controller and action
 
-  resources :articles
+  resources :articles do
+    # creates comments as a nested resources within articles
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
