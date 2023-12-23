2b21b366195063e30f46d4fc14a70f5122e358e5
bmanara <bmacraze@gmail.com>
2023-12-24 01:51:25 +0800

    Add functionality to add articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # Validates the inputs when trying to create in db
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
